7ae000a171084b73a059bd19876067f01c34dd0d
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-07-29 09:08:59 -0300

    Create types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+export type Tools = {
+  imgSrc: string;
+  alt: string;
+  text: string;
+  description: string;
+};
+
+export type SocialIcons = {
+  imgSrc: string;
+  alt: string;
+  href: string;
+}
+
+export type MenuItems = {
+  text: string;
+  href: string;
+}
